3fe77e4accc78b8fea26390af7aafe25cd5677e3
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-13 01:16:20 +0800

    react navigation hasuuuulgit add .

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./view/Login";
+import Dashboard from "./view/Dashboard";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View className="flex flex-1 justify-center items-center">
-      <Text className="text-center font-bold text-3xl tracking-tighter">
-        HELLO WORLD
-      </Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
