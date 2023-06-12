391872f0179d25608d51a80743395b0ccac23f1e
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-13 02:39:32 +0800

    registration styling

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -17,6 +17,14 @@ const RegisterForm = () => {
   return (
     <View className="flex items-center justify-center h-full">
       <View className="mb-4">
+        <Text className="font-bold text-2xl tracking-tight">Student ID:</Text>
+        <TextInput
+          className="border-2 border-gray-500 rounded px-3 py-2"
+          placeholder="ID #"
+          style={{ width: 200 }}
+        />
+      </View>
+      <View className="mb-4">
         <Text className="font-bold text-2xl tracking-tight">Username:</Text>
         <TextInput
           className="border-2 border-gray-500 rounded px-3 py-2"
